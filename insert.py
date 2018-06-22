49c155f89f0edfa1f07b49ed15c5d39ad5d8fbdf
Andrew Hase <oldm8reggie@gmail.com>
2018-06-22 11:26:28 +1000

    gather details correctly pumps into obj

--- insert.py
+++ insert.py
@@ -9,3 +9,21 @@ class Menu(object):
     
 
 
+""" OK Read the files for some variables """
+
+details = open("details.txt","r")
+lines = details.readlines()
+print(lines[0])
+print(lines[1])
+print(lines[2])
+menuTitles = lines[0].split(',')
+menuLinks = lines[1].split(',')
+menuSlugs = lines[2].split(',')
+""" folderTitle = input('Enter the folder name: ') """
+
+test = Menu(menuTitles[0],'.../009','[fusion builder]')
+print(test.titles)
+
+details.close()
+
+
