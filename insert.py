b18a5c67cbfcd1f905ed32a0af17525df4c9c993
Andrew Hase <oldm8reggie@gmail.com>
2018-06-22 10:46:50 +1000

    Im gonna have to build a lot of menus

---
+++ insert.py
@@ -0,0 +1,11 @@
+class Menu(object):
+    titles:[]
+    links:[]
+    slugs:[]
+    def __init__(self,titles,links,slugs):
+     self.titles = titles
+     self.links = links
+     self.slugs = slugs
+    
+
+
