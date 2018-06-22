e6502582dc9a6bf03581c31c0f4edce21f995c1f
Andrew Hase <oldm8reggie@gmail.com>
2018-06-22 13:18:32 +1000

    Why did I have a class? Currently Menu will be constructed

--- insert.py
+++ insert.py
@@ -1,17 +1,19 @@
+from menu import *
+
+
+
 class Menu(object):
-    titles:[]
-    links:[]
-    slugs:[]
-    def __init__(self,titles,links,slugs):
-     self.titles = titles
-     self.links = links
-     self.slugs = slugs
-    
+    titles: []
+    links: []
+
+    def __init__(self, titles, links):
+        self.titles = titles
+        self.links = links
 
 
 """ OK Read the files for some variables """
 
-details = open("details.txt","r")
+details = open("details.txt", "r")
 lines = details.readlines()
 print(lines[0])
 print(lines[1])
@@ -19,11 +21,22 @@ print(lines[2])
 menuTitles = lines[0].split(',')
 menuLinks = lines[1].split(',')
 menuSlugs = lines[2].split(',')
+details.close()
 """ folderTitle = input('Enter the folder name: ') """
 
-test = Menu(menuTitles[0],'.../009','[fusion builder]')
-print(test.titles)
+if len(menuTitles) != len(menuLinks) and len(menuSlugs):
+    print('All Arrays should be equal length, please go back and check arrays')
 
-details.close()
+
+for title, link in zip(menuTitles, menuLinks):
+    Menu(title, link)
+    output = open("menuCreate.html", "w+")
+    output.write(menuTopHtml)
+    for title, link in zip(menuTitles, menuLinks):
+        output.write('<li><a href="' + link + '" class="alert-link">'+title+'</a></li>')
+    output.write(menuBottomHtml)
+    output.close()
+
+""" Create the slugs and thier pages """
 
 
---
+++ menu.py
@@ -0,0 +1,21 @@
+menuTopHtml = """
+
+
+
+
+            <div class="col-4">
+                <div class="alert alert-info" role="alert">
+                  <h4>Menu</h4>
+                    <ul class="list-unstyled">
+                    """
+
+                        
+                 
+
+menuBottomHtml = """      
+   </ul>
+                </div>
+            </div>
+            """
+
+
